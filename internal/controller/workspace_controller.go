4a854f8cb8ed140e77de747feff4020bfeeae762
Chung-Hsuan Tsai <paul_tsai@phison.com>
2026-03-01 01:11:33 +0800

    refactor: update RBAC permissions in role.yaml and workspace_controller.go; remove 'escalate' verb from clusterroles (#20)

--- internal/controller/workspace_controller.go
+++ internal/controller/workspace_controller.go
@@ -69,7 +69,7 @@ type WorkspaceReconciler struct {
 // +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch
 // +kubebuilder:rbac:groups=networking.k8s.io,resources=networkpolicies,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=bind;escalate,resourceNames=admin;edit;view
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=bind,resourceNames=admin;edit;view
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings,verbs=get;list;watch
 // +kubebuilder:rbac:groups=security.istio.io,resources=authorizationpolicies;peerauthentications,verbs=get;list;watch;create;update;patch;delete
 
