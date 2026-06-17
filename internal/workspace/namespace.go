402e9ef6d1fc311bf75ea8fa9de0dcbefb1b7f08
Chung-Hsuan Tsai <paul_tsai@phison.com>
2026-06-17 21:36:26 +0800

    Revert "remove pod-security restrict (#114)" (#120)
    This reverts commit a3092c88c7c986589efffcaf201b694226b01d27.

--- internal/workspace/namespace.go
+++ internal/workspace/namespace.go
@@ -37,8 +37,9 @@ import (
 // without blocking them.
 func podSecurityLabels() map[string]string {
 	return map[string]string{
-		"pod-security.kubernetes.io/warn":  "restricted",
-		"pod-security.kubernetes.io/audit": "restricted",
+		"pod-security.kubernetes.io/enforce": "baseline",
+		"pod-security.kubernetes.io/warn":    "restricted",
+		"pod-security.kubernetes.io/audit":   "restricted",
 	}
 }
 
